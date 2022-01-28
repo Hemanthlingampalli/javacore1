92941b5f7ea67cce18df01f4434f01ef9a6427a7
Hemanthlingampalli <hemanthlingampallihii@gmail.com>
2022-01-28 14:59:14 +0530

    commite the code

---
+++ javaproject/src/javaproject/demo.java
@@ -0,0 +1,13 @@
+package javaproject;
+
+public class demo {
+
+	public static void main(String[] args) {
+		String s= "500";
+		int n = Integer.parseInt(s);
+		System.out.println(n);
+		
+	}
+	
+
+}
