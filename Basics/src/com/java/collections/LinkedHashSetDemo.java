d55fb23588ffcee1f62ba641c2f4236ee8d01101
Hemanthlingampalli <hemanthlingampallihii@gmail.com>
2022-02-03 01:10:57 +0530

    Methods,Collections,Maps,Constructors

---
+++ Basics/src/com/java/collections/LinkedHashSetDemo.java
@@ -0,0 +1,32 @@
+package com.java.collections;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+		LinkedHashSet<String> set=new LinkedHashSet<String>();
+		
+		set.add("apple");
+		set.add("banana");
+		set.add(null);
+		set.add("banana");
+		set.add("cherry");
+		set.add("almond");
+		
+		System.out.println("Size: "+set.size());
+		System.out.println(set);
+		
+		System.out.println("Contains "+set.contains("cherry"));
+		
+		set.remove(null);
+		
+		System.out.println(set);
+		
+		//iterate using iterator 
+		//iterate using for each Loop
+		
+	}
+}
+
